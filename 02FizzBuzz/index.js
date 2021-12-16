766513cf7327777ee15b3381d13c4c61a3c58f94
anto-88 <anto-88@pop-os.localdomain>
2021-12-16 11:04:03 +0100

    testing katas

--- 02FizzBuzz/index.js
+++ 02FizzBuzz/index.js
@@ -1,11 +1,18 @@
-for(i = 0; i<100; i++){
-    if(i%5 === 0 && i%3 ===0){
-       return i += "" + "FIZZBUZZ";
-     }
-     else if(i%5 === 0){
-         return i += "" + "BUZZ";
-     }
-     else if(i%3 === 0){
-         return i += "" + "FIZZ";
-     }
-};
+module.exports = function fizzbuzz() {
+
+    for(let i = 1; i<=100; i++){
+
+        if(i%5 === 0 && i%3 === 0){
+       return "FIZZBUZZ" }
+        
+        else if(i%5 === 0){
+       return "BUZZ"}
+       
+        else if(i%3 === 0){
+       return "FIZZ"}
+      
+        else{
+          return i ;
+    }
+}
+}
---
+++ 02FizzBuzz/test/index.test.js
@@ -0,0 +1,12 @@
+
+const chai = require('chai');
+const assert = chai.assert;
+const fizzbuzz = require('../index');
+
+describe("boucle modulo",function(){
+
+    it(" type ", function(){
+        assert.equal(fizzbuzz() === 1);
+    });
+
+});
